daffaba0faac618df42050230a6f47e24c167c7a
TheGreatOracle <thisisseva@yandex.ru>
2019-02-24 16:29:36 +0300

    working!

--- js/main.js
+++ js/main.js
@@ -1,150 +1,117 @@
-//this game will have only 1 state
 var GameState = {
-  //load the game assets before the game starts
   preload: function() {
-    this.load.spritesheet('chicken', 'assets/images/chicken_spritesheet.png', 131, 200, 3);
-    
-    this.load.spritesheet('horse', 'assets/images/horse_spritesheet.png', 212, 200, 3);
-    this.load.spritesheet('pig', 'assets/images/pig_spritesheet.png', 297, 200, 3);
-    this.load.spritesheet('sheep', 'assets/images/sheep_spritesheet.png', 244, 200, 3);  
-    this.load.image('arrow', 'assets/images/arrow.png');
-
-    this.load.audio('chickenSound', ['assets/audio/chicken.ogg', 'assets/audio/chicken.mp3']);
-    this.load.audio('horseSound', ['assets/audio/horse.ogg', 'assets/audio/horse.mp3']);
-    this.load.audio('pigSound', ['assets/audio/pig.ogg', 'assets/audio/pig.mp3']);
-    this.load.audio('sheepSound', ['assets/audio/sheep.ogg', 'assets/audio/sheep.mp3']);
+this.game.physics.startSystem(Phaser.Physics.ARKADE); 
+this.cursors = this.game.input.keyboard.createCursorKeys(); 
   },
 
   init: function() {
-    //scaling options
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
+
   },
-  //executed after everything is loaded
-  create: function() {
-
-    //this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
-
-    //this.scale.pageAlignHorizontally = true;
-    //this.scale.pageAlignVertically = true;
-
-    this.background = this.game.add.sprite(0,0,'background')
-
-    var animalData = [
-      {key: 'chicken', text:'CHIKEN', audio: 'chickenSound'},
-      {key: 'horse', text:'HORSE', audio: 'horseSound'},
-      {key: 'pig', text:'PIG', audio: 'pigSound'},
-      {key: 'sheep', text:'SHEEP', audio: 'sheepSound'},
-    ]
-
-    this.animals = this.game.add.group()
-
-    var self = this
-    var animal
-
-    animalData.forEach((element) => {
-      animal = self.animals.create(-1000, this.game.world.centerY, element.key)
-      animal.customParams = {text: element.text, sound: self.game.add.audio(element.audio)}
-      animal.anchor.setTo(0.5)
-
-      animal.animations.add('animate', [0,1,2,1,0], 3, false)
-
-      animal.inputEnabled = true
-      animal.input.pixelPerfectClick = true
-      animal.events.onInputDown.add(this.animateAnimal, self)
-
-    })
-
-    this.currentAnimal = this.animals.next()
-    this.currentAnimal.position.set(this.game.world.centerX, this.game.world.centerY)
-
-    this.showText(this.currentAnimal)
-
-    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY,'arrow')
-    this.rightArrow.anchor.setTo(0.5)
-    this.rightArrow.customParams = {direction: -1}
-
-    this.rightArrow.inputEnabled = true
-    this.rightArrow.input.pixelPerfectClick = true
-    this.rightArrow.events.onInputDown.add(this.switchAnimal, this)
-
-    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY,'arrow')
-    this.leftArrow.scale.x = -1
-    this.leftArrow.anchor.setTo(0.5)
-    this.leftArrow.customParams = {direction: 1}
-
-    this.leftArrow.inputEnabled = true
-    this.leftArrow.input.pixelPerfectClick = true
-    this.leftArrow.events.onInputDown.add(this.switchAnimal, this)
-  },
-  //this is executed multiple times per second
-  update: function() {
+  create: function(){
+    game.stage.backgroundColor = "#33ffff";
+this.blocks = ['green', 'yellow', 'red', 'blue']
+this.k = 3;
+this.delay = 1000;
+this.timer = 0;
+this.fire = false;
+this.right = 100;
+this.left = 100;
+this.game = 1;
+  // console.log(Phaser.Keyboard.A)
+  //console.log(this.game.input.keyboard)
+  this.keyA = this.input.keyboard.addKey(65);
+  this.keyW = this.input.keyboard.addKey(87);
+  this.keyS = this.input.keyboard.addKey(83);
+  this.keyD = this.input.keyboard.addKey(68);
   },
 
-  switchAnimal: function(sprite, event) {
+  update: function() {  
 
-    if(this.isMoving) {
-      return false
-    } 
-
-    this.isMoving = true
-
-    this.animalText.visible = false
-
-    var newAnimal, endX
-
-    if(sprite.customParams.direction > 0) {
-      newAnimal = this.animals.next()
-      newAnimal.x = -newAnimal.width/2
-      endX = 640 + this.currentAnimal.width/2
-    } else {
-      newAnimal = this.animals.previous()
-      newAnimal.x = 640 + newAnimal.width/2
-      endX = -this.currentAnimal.width/2
+    // if(this.cursors.up.isDown) {
+    //   console.log("Input!")
+    // } else if (this.keyA.isDown) {
+    //   console.log('Ili')
+    // }
+    if (this.game == 1){
+      this.timer++;
     }
-
-    var newAnimalMovement = game.add.tween(newAnimal)
-    newAnimalMovement.to({x: this.game.world.centerX}, 1000)
-    newAnimalMovement.onComplete.add(() => {
-      this.isMoving = false
-      this.showText(newAnimal)
-    }, this)
-    newAnimalMovement.start()
-
-    var currentAnimalMovement = this.game.add.tween(this.currentAnimal)
-    currentAnimalMovement.to({x: endX}, 1000)
-    currentAnimalMovement.start()
-
-    this.currentAnimal = newAnimal
-
-  },
-
-  showText: function (animal) {
-    if(!this.animalText) {
-      var style = {
-        font: 'bold 30px Arial',
-        fill: 'red',
-        align: 'center'
+    
+    // console.log('правые: ', this.right);
+    // console.log('левые: ', this.left);
+    // console.log(this.timer)
+    if(this.timer > 100 && this.game == 1) {
+      var randNumber = Math.random();
+      if(randNumber > 0.995 /*&& this.fire === false*/){
+        this.fire = true;
+        this.l = Math.floor(Math.random() * 4);
+        if (this.l == 0){
+          console.log('!Вверх!');
+        } else if (this.l == 1){
+          console.log('!Вправо!')
+        } else if (this.l == 2){
+          console.log('!Лево!')
+        } else if ( this.l == 3){
+          console.log('!вниз!');
+        }
       }
-      this.animalText = this.game.add.text(this.game.width/2, this.game.height * 0.85, '', style);
-      this.animalText.anchor.setTo(0.5)
+        // this.timer = 0;
+        
+        if (this.l == 0 && this.cursors.up.isDown && this.fire === true){
+          console.log('Правый ранил')
+          // this.right += 50;
+          this.left -= 50;
+          this.fire = false;
+        } else if (this.l == 0 && this.keyW.isDown && this.fire === true){
+        console.log('Левый ранил')
+          this.right -= 50;
+          // this.left += 50;
+          this.fire = false;
+      }
+              if (this.l == 1 && this.cursors.right.isDown && this.fire === true){
+          console.log('Правый ранил')
+          // this.right += 50;
+          this.left -= 50;
+          this.fire = false;
+        } else if (this.l == 1 && this.keyD.isDown && this.fire === true){
+        console.log('Левый ранил')
+          this.right -= 50;
+          // this.left += 50; 
+          this.fire = false;       
+      }
+              if (this.l == 2 && this.cursors.left.isDown && this.fire === true){
+          console.log('Правый ранил')
+          // this.right += 50;
+          this.left -= 50;
+          this.fire = false;
+        } else if (this.l == 2 && this.keyA.isDown && this.fire === true){
+        console.log('Левый ранил')
+          this.right -= 50;
+          // this.left += 50;
+          this.fire = false;    
+      }
+              if (this.l == 3 && this.cursors.down.isDown && this.fire === true){
+          console.log('Правый ранил')
+          // this.right += 50;
+          this.left -= 50;
+          this.fire = false;
+        } else if (this.l == 3 && this.keyS.isDown && this.fire === true){
+        console.log('Левый ранил')
+          this.right -= 50;
+          // this.left += 50;
+          this.fire = false;
+      }
     }
-
-    this.animalText.setText(animal.customParams.text)
-    this.animalText.visible = true
-  },
-
-  animateAnimal: function(sprite, event) {
-    sprite.play('animate')
-    sprite.customParams.sound.play()
-  }
-  
-
-};
-
+    if (this.right == 0 && this.game == 1){
+       this.game = 0;
+       console.log('Левый вин')
+    } else if (this.left == 0 && this.game == 1){
+       this.game = 0;
+       console.log('Правый вин');
+    }
+    },
+  };
 //initiate the Phaser framework
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
+var game = new Phaser.Game(1080, 464, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
